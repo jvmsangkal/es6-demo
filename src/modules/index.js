a3ac5b9ae1fd1efe55992bdae716dbd1f37b49d3
John Viscel Sangkal <jv@codeninja.co>
2018-04-23 21:55:08 +0800

    add samples for modules

---
+++ src/modules/index.js
@@ -0,0 +1,5 @@
+import * as math from './math'
+import { ln, e } from './mathplusplus'
+
+console.log(math.add(1, 2))
+console.log(ln(e))
---
+++ src/modules/mathplusplus.js
@@ -0,0 +1,7 @@
+export * from './math'
+
+export const e = 2.71828182846
+
+export function ln(x) {
+  return Math.log(x)
+}
